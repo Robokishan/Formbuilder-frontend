8cf13ae965a36c03560f9077507b80268747076b
Kishan Joshi <robokishan.blogspot@gmail.com>
2021-08-01 16:15:28 +0530

    Update Registration.jsx

--- src/views/v1/Registration.jsx
+++ src/views/v1/Registration.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { doRegister } from "../../actions/auth";
 import { useDispatch } from "react-redux";
 import {
