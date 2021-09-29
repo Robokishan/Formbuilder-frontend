191c0727c88aa1b9e13302800da38d4e20ba87ad
Kishan Joshi <kishan.j@simformsolutions.com>
2021-09-29 18:42:44 +0530

    fix: removed unnecessary lines

--- src/views/v1/__test__/Auth.test.js
+++ src/views/v1/__test__/Auth.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Registration from "../Registration.jsx";
